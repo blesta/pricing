d5d762a9b6f0909e53b1fe956d21de34a27461c7
clphillips <cody@phillipsdata.com>
2015-04-16 15:56:40 -0700

    Added TaxPrice

--- src/TaxPrice.php
+++ src/TaxPrice.php
@@ -3,21 +3,27 @@
 /**
  *
  */
-class TaxPrice extends AbstractPriceModifer
+class TaxPrice extends AbstractPriceModifier
 {
 
     public function off($price)
     {
-
+        if ('inclusive' == $this->type) {
+            return $price - $this->on($price);
+        }
+        return $price;
     }
 
     public function on($price)
     {
-
+        return max(0, $this->amount / 100) * $price;
     }
 
     public function including($price)
     {
-
+        if ('exclusive' == $this->type) {
+            return $price + $this->on($price);
+        }
+        return $price;
     }
 }
---
+++ tests/TaxPriceTest.php
@@ -0,0 +1,101 @@
+<?php
+
+class TaxPriceTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers TaxPrice::__construct
+     */
+    public function testConstruct()
+    {
+        $this->assertInstanceOf('TaxPrice', new TaxPrice(10.00, 'exclusive'));
+    }
+
+    /**
+     * @covers TaxPrice::off
+     * @uses TaxPrice::on
+     * @dataProvider offProvider
+     */
+    public function testOff($tax, $price, $price_after)
+    {
+        $this->assertEquals($price_after, $tax->off($price));
+    }
+
+    /**
+     * Data provider
+     *
+     * @return array
+     */
+    public function offProvider()
+    {
+        return array(
+            array(new TaxPrice(0, 'exclusive'), 10.00, 10.00),
+            array(new TaxPrice(50, 'exclusive'), 10.00, 10.00),
+            array(new TaxPrice(100, 'exclusive'), 10.00, 10.00),
+            array(new TaxPrice(-100, 'exclusive'), 10.00, 10.00),
+
+            array(new TaxPrice(0, 'inclusive'), 10.00, 10.00),
+            array(new TaxPrice(50, 'inclusive'), 10.00, 5.00),
+            array(new TaxPrice(100, 'inclusive'), 10.00, 0.00),
+            array(new TaxPrice(-100, 'inclusive'), 10.00, 10.00)
+        );
+    }
+
+    /**
+     * @covers TaxPrice::on
+     * @dataProvider onProvider
+     */
+    public function testOn($tax, $price, $tax_amount)
+    {
+        $this->assertEquals($tax_amount, $tax->on($price));
+    }
+
+    /**
+     * Data provider
+     *
+     * @return array
+     */
+    public function onProvider()
+    {
+        return array(
+            array(new TaxPrice(0, 'exclusive'), 10.00, 0.00),
+            array(new TaxPrice(50, 'exclusive'), 10.00, 5.00),
+            array(new TaxPrice(100, 'exclusive'), 10.00, 10.00),
+            array(new TaxPrice(-100, 'exclusive'), 10.00, 0.00),
+
+            array(new TaxPrice(0, 'inclusive'), 10.00, 0.00),
+            array(new TaxPrice(50, 'inclusive'), 10.00, 5.00),
+            array(new TaxPrice(100, 'inclusive'), 10.00, 10.00),
+            array(new TaxPrice(-100, 'inclusive'), 10.00, 0.00)
+        );
+    }
+
+    /**
+     * @covers TaxPrice::including
+     * @uses TaxPrice::on
+     * @dataProvider includingProvider
+     */
+    public function testIncluding($tax, $price, $result)
+    {
+        $this->assertEquals($result, $tax->including($price));
+    }
+
+    /**
+     * Data provider
+     *
+     * @return array
+     */
+    public function includingProvider()
+    {
+        return array(
+            array(new TaxPrice(0, 'exclusive'), 10.00, 10.00),
+            array(new TaxPrice(50, 'exclusive'), 10.00, 15.00),
+            array(new TaxPrice(100, 'exclusive'), 10.00, 20.00),
+            array(new TaxPrice(-100, 'exclusive'), 10.00, 10.00),
+
+            array(new TaxPrice(0, 'inclusive'), 10.00, 10.00),
+            array(new TaxPrice(50, 'inclusive'), 10.00, 10.00),
+            array(new TaxPrice(100, 'inclusive'), 10.00, 10.00),
+            array(new TaxPrice(-100, 'inclusive'), 10.00, 10.00)
+        );
+    }
+}
