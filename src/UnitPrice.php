bcdc906b57700528765c8b22509172cd389a259e
Tyson <Tyson@Tyson-PC>
2015-04-23 10:01:15 -0700

    Added doc comments to UnitPrice and updated tests

--- src/UnitPrice.php
+++ src/UnitPrice.php
@@ -1,29 +1,57 @@
 <?php
 
 /**
- *
+ * Builds a unit price
  */
 class UnitPrice extends AbstractPriceDescription
 {
+    /**
+     * @var float The unit price
+     */
     protected $price;
+
+    /**
+     * @var int The quantity of unit prices
+     */
     protected $qty;
 
+    /**
+     * Initialize the unit price
+     *
+     * @param float $price The unit price
+     * @param int $qty The quantity of unit prices (optional, default 1)
+     */
     public function __construct($price, $qty = 1)
     {
         $this->price = $price;
         $this->qty = $qty;
     }
 
+    /**
+     * Retrieves the set unit price
+     *
+     * @return float The unit price
+     */
     public function price()
     {
         return $this->price;
     }
 
+    /**
+     * Retrieves the set quantity
+     *
+     * @return int The quantity
+     */
     public function qty()
     {
         return $this->qty;
     }
 
+    /**
+     * Retrieves the total price
+     *
+     * @return float The total price considering quantity
+     */
     public function total()
     {
         return $this->qty * $this->price;
