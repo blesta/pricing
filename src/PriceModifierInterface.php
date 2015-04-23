0ec21420b8f100d4fd1e2df695c8c3f97dcdf700
Tyson <Tyson@Tyson-PC>
2015-04-23 12:38:39 -0700

    Added doc comments to the PriceModifierInterface

--- src/PriceModifierInterface.php
+++ src/PriceModifierInterface.php
@@ -1,13 +1,45 @@
 <?php
 
 /**
- *
+ * Interface for price modifications
  */
 interface PriceModifierInterface extends PriceDescriptionInterface
 {
+    /**
+     * Initializes a price modifier
+     *
+     * @param float $amount The price modifier amount
+     * @param string $type The price modifier type
+     */
     public function __construct($amount, $type);
+
+    /**
+     * Retrieves the price modifier amount
+     *
+     * @return float The amount
+     */
     public function amount();
+
+    /**
+     * Retrieves the price modifier type
+     *
+     * @return string The type
+     */
     public function type();
+
+    /**
+     * Retrieves the price after modification
+     *
+     * @param float $price The price to modify
+     * @return float The price after modification
+     */
     public function off($price);
+
+    /**
+     * Retrieves the modification price
+     *
+     * @param float $price The price to modify
+     * @return float The modification price
+     */
     public function on($price);
 }
