c533bfbc97d9b1595e11d383cb5f843fb8630179
Tyson <Tyson@Tyson-PC>
2015-04-23 09:46:03 -0700

    Added doc comments to AbstractPriceDescription, and included tests

--- src/AbstractPriceDescription.php
+++ src/AbstractPriceDescription.php
@@ -1,17 +1,30 @@
 <?php
 
 /**
- *
+ * Abstract Price Description for setting a price description
  */
 abstract class AbstractPriceDescription implements PriceDescriptionInterface
 {
+    /**
+     * @var string The price description
+     */
     protected $description;
 
+    /**
+     * Sets a description for a price
+     *
+     * @param string $description The price description
+     */
     public function setDescription($description)
     {
         $this->description = $description;
     }
 
+    /**
+     * Retrieves the price description
+     *
+     * @return string The price description
+     */
     public function getDescription()
     {
         return $this->description;
