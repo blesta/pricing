f0b46fb67bac50c47a6657e1435ed49888b1e39f
Tyson <Tyson@Tyson-PC>
2015-04-24 15:33:47 -0700

    Further implemented Iterator and total calculations, including tests. Amount calculations related to discounts are still incomplete

--- src/ItemPriceCollection.php
+++ src/ItemPriceCollection.php
@@ -19,16 +19,19 @@ class ItemPriceCollection implements PriceTotalInterface, Iterator
      * Adds an ItemPrice to the collection
      *
      * @param ItemPrice $price An item to add to the collection
+     * @return reference to this
      */
     public function append(ItemPrice $price)
     {
         $this->collection[] = $price;
+        return $this;
     }
 
     /**
      * Removes an ItemPrice from the collection
      *
      * @param ItemPrice $price An item to remove from the collection
+     * @return reference to this
      */
     public function remove(ItemPrice $price)
     {
@@ -39,7 +42,7 @@ class ItemPriceCollection implements PriceTotalInterface, Iterator
             }
         }
 
-        #$this->collection = array_values($this->collection);
+        return $this;
     }
 
     /**
@@ -52,42 +55,146 @@ class ItemPriceCollection implements PriceTotalInterface, Iterator
         return count($this->collection);
     }
 
-
-    // PriceTotalInterface
+    /**
+     * Retrieves the total price of all items within the collection including taxes without discounts
+     *
+     * @return float The total price including taxes without including discounts
+     */
     public function totalAfterTax()
     {
+        $total = 0;
+        foreach ($this->collection as $item) {
+            $total += $item->totalAfterTax();
+        }
 
+        return $total;
     }
+
+    /**
+     * Retrieves the total price of all items within the collection including discounts without taxes
+     *
+     * @return float The total price including discounts without including taxes
+     */
     public function totalAfterDiscount()
     {
+        #
+        # TODO: decrease and reset discount price if of 'amount' type
+        #
+        $total = 0;
+        foreach ($this->collection as $item) {
+            $total += $item->totalAfterDiscount();
+        }
 
+        return $total;
     }
+
+    /**
+     * Retrieves the subtotal of all items within the collection
+     *
+     * @return float The subtotal of all items in the collection
+     */
     public function subtotal()
     {
-        // all item total() values without any tax or discounts
+        // Sum the subtotals of each ItemPrice
+        $total = 0;
+        foreach ($this->collection as $item_price) {
+            $total += $item_price->subtotal();
+        }
+
+        return $total;
     }
+
+    /**
+     * Retrieves the total of all items within the collection
+     *
+     * @return float The total of all items in the collection
+     */
     public function total()
     {
+        #
+        # TODO: decrease and reset discount price if of 'amount' type
+        #
+        // Sum the totals of each ItemPrice
+        $total = 0;
+        foreach ($this->collection as $item_price) {
+            $total += $item_price->total();
+        }
 
+        return $total;
     }
-    public function taxAmount(TaxPrice $tax =  null)
+
+    /**
+     * Retrieves the total tax amount for all items within the collection
+     *
+     * @param TaxPrice $tax
+     */
+    public function taxAmount(TaxPrice $tax = null)
     {
+        $total = 0;
+        foreach ($this->collection as $item_price) {
+            $total += $item_price->taxAmount($tax);
+        }
 
+        return $total;
     }
-    public function discountAmount(DiscountPrice $discount =  null)
+
+    /**
+     * Retrieves the total discount amount for all items within the collection
+     *
+     * @param DiscountPrice $discount
+     */
+    public function discountAmount(DiscountPrice $discount = null)
     {
-
+        #
+        # TODO: decrease and reset discount price if of 'amount' type
+        #
     }
+
+    /**
+     * Retrieves a list of all unique TaxPrice objects apart of this collection
+     *
+     * @return array An array of TaxPrice objects
+     */
     public function taxes()
     {
-        // return array of TaxPrice
-    }
-    public function discounts()
-    {
-        // return array of DiscountPrice
+        // Include unique instances of TaxPrice
+        $taxes = array();
+        foreach ($this->collection as $item_price) {
+            foreach ($item_price->taxes() as $tax_price) {
+                if (!in_array($tax_price, $taxes, true)) {
+                    $taxes[] = $tax_price;
+                }
+            }
+        }
+
+        return $taxes;
     }
 
-    // Iterator
+    /**
+     * Retrieves a list of all unique DiscountPrice objects apart of this collection
+     *
+     * @return array An array of DiscountPrice objects
+     */
+    public function discounts()
+    {
+        // Include unique instances of DiscountPrice
+        $discounts = array();
+        foreach ($this->collection as $item_price) {
+            foreach ($item_price->discounts() as $discount_price) {
+                if (!in_array($discount_price, $discounts, true)) {
+                    $discounts[] = $discount_price;
+                }
+            }
+        }
+
+        return $discounts;
+    }
+
+    /**
+     * Retrieves the item in the collection at the current pointer
+     *
+     * @return mixed The ItemPrice in the collection at the current position, otherwise null
+     */
     public function current()
     {
         return (
@@ -96,24 +203,59 @@ class ItemPriceCollection implements PriceTotalInterface, Iterator
             : null
         );
     }
+
+    /**
+     * Retrieves the index currently being pointed at in the collection
+     *
+     * @return int The index of the position in the collection
+     */
     public function key()
     {
         return $this->position;
     }
+
+    /**
+     * Moves the pointer to the next item in the collection
+     */
     public function next()
     {
-        // Set the next position to the next entry in the collection
-        $position = ++$this->position;
-        if (false !== next($this->collection)) {
-            $this->position = key($this->collection);
+        // Set the next position to the position of the next item in the collection
+        $position = $this->position;
+        foreach ($this->collection as $index => $item) {
+            if ($index > $position) {
+                $this->position = $index;
+                break;
+            }
+        }
+
+        // If there is no next item in the collection, increment the position instead
+        if ($position == $this->position) {
+            ++$this->position;
         }
     }
+
+    /**
+     * Moves the pointer to the first item in the collection
+     */
     public function rewind()
     {
-        // Set the current position to the first entry in the collection
+        // Reset the array pointer to the first entry in the collection
         reset($this->collection);
-        $this->position = key($this->collection);
+
+        // Set the position to the first entry in the collection if there is one
+        $first_index = key($this->collection);
+        $this->position = (
+            $first_index === null
+            ? 0
+            : $first_index
+        );
     }
+
+    /**
+     * Determines whether the current pointer references a valid item in the collection
+     *
+     * @return boolean True if the pointer references a valid item in the collection, false otherwise
+     */
     public function valid()
     {
         return array_key_exists($this->position, $this->collection);
--- tests/ItemPriceCollectionTest.php
+++ tests/ItemPriceCollectionTest.php
@@ -8,65 +8,413 @@ class ItemPriceCollectionTest extends PHPUnit_Framework_TestCase
 
     /**
      * @covers ::append
+     * @uses ::count
      */
     public function testAppend()
     {
         $collection = new ItemPriceCollection();
         $items = array(new ItemPrice(10, 2), new ItemPrice(5, 1));
 
-        #
-        # TODO: replace manual count with $collection->count()
-        #
         // Add 1 item
         $collection->append($items[0]);
-        $num_items = 0;
         foreach ($collection as $item_price) {
-            $this->assertSame($items[$num_items], $item_price);
-            $num_items++;
+            $this->assertSame($items[0], $item_price);
         }
-        $this->assertEquals(1, $num_items);
+        $this->assertEquals(1, $collection->count());
 
         // Add a second item
         $collection->append($items[1]);
-        $num_items = 0;
+        $i = 0;
         foreach ($collection as $item_price) {
-            $this->assertSame($items[$num_items], $item_price);
-            $num_items++;
+            $this->assertSame($items[$i++], $item_price);
         }
-        $this->assertEquals(2, $num_items);
+        $this->assertEquals(2, $collection->count());
     }
 
     /**
      * @covers ::append
      * @covers ::remove
+     * @uses ::count
      */
     public function testRemove()
     {
         $collection = new ItemPriceCollection();
+
+        // Add 4 items to the collection
         $item1 = new ItemPrice(2, 1);
         $items = array(new ItemPrice(1, 2), $item1, new ItemPrice(3, 1), $item1);
-
         foreach ($items as $item) {
             $collection->append($item);
         }
 
-        #
-        # TODO: replace manual count with $collection->count()
-        #
         // Remove an item, leaving 3 remaining
         $collection->remove($items[0]);
-        $num_items = 0;
         foreach ($collection as $item_price) {
-            $num_items++;
+            $this->assertNotSame($items[0], $item_price);
         }
-        $this->assertEquals(3, $num_items);
+        $this->assertEquals(3, $collection->count());
 
         // Remove an item that exists multiple times
         $collection->remove($item1);
-        $num_items = 0;
         foreach ($collection as $item_price) {
-            $num_items++;
+            $this->assertNotSame($item1, $item_price);
         }
-        $this->assertEquals(1, $num_items);
+        $this->assertEquals(1, $collection->count());
+    }
+
+    /**
+     * @covers ::count
+     * @uses ::append
+     * @uses ::remove
+     */
+    public function testCount()
+    {
+        // No items, count is zero
+        $collection = new ItemPriceCollection();
+        $this->assertEquals(0, $collection->count());
+
+        // One item, count is one
+        $item = new ItemPrice(10, 1);
+        $collection->append($item);
+        $count = 1;
+        $this->assertEquals($count, $collection->count());
+
+        // Multiple items, count increasing by one each time
+        for ($i=0; $i<10; $i++) {
+            $collection->append(new ItemPrice($i, 1));
+            $this->assertEquals(++$count, $collection->count());
+        }
+
+        // Removing an item, the count decreases by one
+        $collection->remove($item);
+        $this->assertEquals(--$count, $collection->count());
+    }
+
+    /**
+     * @covers ::totalAfterTax
+     * @dataProvider totalProvider
+     */
+    public function testTotalAfterTax(ItemPriceCollection $collection, array $expected_totals)
+    {
+        $this->assertEquals($expected_totals['total_with_tax'], $collection->totalAfterTax());
+    }
+
+    /**
+     * @covers ::totalAfterDiscount
+     * @dataProvider totalProvider
+     */
+    public function testTotalAfterDiscount(ItemPriceCollection $collection, array $expected_totals)
+    {
+        #
+        # TODO: total is incorrect for discount amounts
+        #
+        $this->assertEquals($expected_totals['total_with_discount'], $collection->totalAfterDiscount());
+    }
+
+    /**
+     * @covers ::subtotal
+     * @dataProvider totalProvider
+     */
+    public function testSubtotal(ItemPriceCollection $collection, array $expected_totals)
+    {
+        $this->assertEquals($expected_totals['subtotal'], $collection->subtotal());
+    }
+
+    /**
+     * @covers ::total
+     * @dataProvider totalProvider
+     */
+    public function testTotal(ItemPriceCollection $collection, array $expected_totals)
+    {
+        #
+        # TODO: total is incorrect for discount amounts. discount amounts are not decreased per item
+        #
+        echo "\nTOTAL:" . $collection->total() . "\n";
+        $this->assertEquals($expected_totals['total'], $collection->total());
+    }
+
+    /**
+     *
+     * @covers ::taxAmount
+     * @dataProvider totalProvider
+     */
+    public function testTaxAmount(ItemPriceCollection $collection, array $expected_totals)
+    {
+        $this->assertEquals($expected_totals['tax'], $collection->taxAmount());
+    }
+
+    /**
+     * @covers ::discountAmount
+     * @dataProvider totalProvider
+     */
+    public function testDiscountAmount()
+    {
+        #
+        # TODO: consider discounts over multiple items
+        #
+    }
+
+    /**
+     * @covers ::taxes
+     * @dataProvider totalProvider
+     */
+    public function testTaxes(ItemPriceCollection $collection, array $expected_totals)
+    {
+        $this->assertContainsOnlyInstancesOf("TaxPrice", $collection->taxes());
+
+        // Exactly each expected tax should exist
+        foreach ($expected_totals['taxes'] as $tax_price) {
+            $this->assertContains($tax_price, $collection->taxes());
+        }
+        $this->assertCount(count($expected_totals['taxes']), $collection->taxes());
+    }
+
+    /**
+     * @covers ::discounts
+     * @dataProvider totalProvider
+     */
+    public function testDiscounts(ItemPriceCollection $collection, array $expected_totals)
+    {
+        $this->assertContainsOnlyInstancesOf("DiscountPrice", $collection->discounts());
+
+        // Exactly each expected discount should exist
+        foreach ($expected_totals['discounts'] as $discount_price) {
+            $this->assertContains($discount_price, $collection->discounts());
+        }
+        $this->assertCount(count($expected_totals['discounts']), $collection->discounts());
+    }
+
+    /**
+     * Data provider for subtotal/total
+     *
+     * @return array
+     */
+    public function totalProvider()
+    {
+        // Item with discounts and tax
+        $tax_price = new TaxPrice(10, 'exclusive');
+        $item1 = new ItemPrice(10, 2);
+        $item1->setDiscount(new DiscountPrice(20, 'percent'));
+        $item1->setDiscount(new DiscountPrice(1, 'amount'));
+        $item1->setTax($tax_price);
+
+        // Item with tax
+        $item2 = new ItemPrice(5.25, 3);
+        $item2->setTax($tax_price);
+
+        // Item with compound tax and discount
+        $item3 = new ItemPrice(100.00, 1);
+        $item3->setDiscount(new DiscountPrice(1.50, 'amount'));
+        $item3->setTax(new TaxPrice(8, 'exclusive'), new TaxPrice(5, 'exclusive'));
+
+        // Amount discount applied to multiple items
+        $discount_price = new DiscountPrice(18.00, 'amount');
+        $item4 = new ItemPrice(15, 1);
+        $item4->setDiscount($discount_price);
+        $item5 = new ItemPrice(5, 1);
+        $item5->setDiscount($discount_price);
+
+        // Set collections of the items
+        $collection1 = new ItemPriceCollection();
+        $collection1->append($item1)->append($item1);
+
+        $collection2 = new ItemPriceCollection();
+        $collection2->append($item1)->append($item2);
+
+        $collection3 = new ItemPriceCollection();
+        $collection3->append($item1)->append($item2)->append($item3);
+
+        $collection4 = new ItemPriceCollection();
+        $collection4->append($item4)->append($item5);
+
+        return array(
+            array($collection1, $this->getItemTotals($item1, $item1)),
+            array($collection2, $this->getItemTotals($item1, $item2)),
+            array($collection3, $this->getItemTotals($item1, $item2, $item3)),
+            array($collection4, $this->getItemTotals($item4, $item5)),
+        );
+    }
+
+    /**
+     * Retrieves total information for a set of items
+     *
+     * @param ItemPrice An ItemPrice object
+     * @param ...
+     * @return array An array of totals combining each item price
+     */
+    private function getItemTotals()
+    {
+        // NOTE: 'total', 'total_with_discount', and 'discount' may be INCORRECT
+        // if a DiscountPrice of type 'amount' applies to multiple items!
+        $totals = array(
+            'subtotal' => 0,
+            'total' => 0,
+            'total_with_tax' => 0,
+            'total_with_discount' => 0,
+            'tax' => 0,
+            'discount' => 0,
+            'taxes' => array(),
+            'discounts' => array()
+        );
+
+        $args = func_get_args();
+        foreach ($args as $item) {
+            $totals['subtotal'] += $item->subtotal();
+            $totals['total'] += $item->total();
+            $totals['total_with_tax'] += $item->totalAfterTax();
+            $totals['total_with_discount'] += $item->totalAfterDiscount();
+            $totals['tax'] += $item->taxAmount();
+            $totals['discount'] += $item->discountAmount();
+            $totals['taxes'] = $this->getUnique($totals['taxes'], $item->taxes());
+            $totals['discounts'] = $this->getUnique($totals['discounts'], $item->discounts());
+        }
+
+        return $totals;
+    }
+
+    /**
+     * Includes unique items from $arr2 into $arr1
+     *
+     * @param array $arr1 An array of objects
+     * @param array $arr2 An array of objects to include
+     * @return array An array of unique objects
+     */
+    private function getUnique($arr1, $arr2)
+    {
+        foreach ($arr2 as $obj) {
+            if (!in_array($obj, $arr1, true)) {
+                $arr1 = array_merge($arr1, array($obj));
+            }
+        }
+
+        return $arr1;
+    }
+
+    /**
+     * @covers ::current
+     * @uses ::append
+     */
+    public function testCurrent()
+    {
+        $collection = new ItemPriceCollection();
+
+        // No items exist, there is no current item
+        $this->assertNull($collection->current());
+
+        // One item
+        $item = new ItemPrice(10, 1);
+        $collection->append($item);
+        $this->assertSame($item, $collection->current());
+
+        // First item is still the current item
+        $collection->append(new ItemPrice(30, 2));
+        $this->assertSame($item, $collection->current());
+    }
+
+    /**
+     * @covers ::key
+     * @covers ::next
+     */
+    public function testKey()
+    {
+        $collection = new ItemPriceCollection();
+
+        // No items exist, but the key should be at the first index
+        $this->assertEquals(0, $collection->key());
+
+        // Key should point at the next index
+        $collection->next();
+        $this->assertEquals(1, $collection->key());
+    }
+
+    /**
+     * @covers ::next
+     * @covers ::rewind
+     * @covers ::current
+     * @covers ::key
+     * @uses ::append
+     * @uses ::remove
+     */
+    public function testNext()
+    {
+        $collection = new ItemPriceCollection();
+
+        // Position starts at 0, increments each time
+        $collection->next();
+        $collection->next();
+        $collection->next();
+        $this->assertEquals(3, $collection->key());
+
+        $collection->rewind();
+        $this->assertEquals(0, $collection->key());
+
+        // Add items, ensure next() iterates to the next item, not just the next index
+        $item1 = new ItemPrice(1, 1);
+        $item2 = new ItemPrice(2, 1);
+        $item3 = new ItemPrice(3, 1);
+        $collection->append($item1)->append($item2)->append($item3);
+        $collection->remove($item2);
+        $this->assertSame($item1, $collection->current());
+
+        $collection->next();
+        $this->assertSame($item3, $collection->current());
+
+        // Remove the current item, and then get the next item
+        $collection->rewind();
+        $this->assertSame($item1, $collection->current());
+        $collection->remove($item1);
+        $collection->next();
+        $this->assertSame($item3, $collection->current());
+
+        // The next item is outside the collection and should be null
+        $collection->next();
+        $this->assertNull($collection->current());
+    }
+
+    /**
+     * @covers ::rewind
+     * @covers ::key
+     * @covers ::next
+     * @uses ::append
+     */
+    public function testRewind()
+    {
+        $collection = new ItemPriceCollection();
+
+        // No items exist
+        $this->assertEquals(0, $collection->key());
+
+        $collection->rewind();
+        $this->assertEquals(0, $collection->key());
+
+        // Increase the position
+        $collection->next();
+        $collection->next();
+        $this->assertEquals(2, $collection->key());
+
+        // Rewind the position back
+        $collection->rewind();
+        $this->assertEquals(0, $collection->key());
+    }
+
+    /**
+     * @covers ::valid
+     * @covers ::next
+     * @uses ::append
+     */
+    public function testValid()
+    {
+        $collection = new ItemPriceCollection();
+
+        // No items exist, position is not valid
+        $this->assertFalse($collection->valid());
+
+        // Item takes first position
+        $collection->append(new ItemPrice(10, 1));
+        $this->assertTrue($collection->valid());
+
+        // No item exists in the next position
+        $collection->next();
+        $this->assertFalse($collection->valid());
     }
 }
