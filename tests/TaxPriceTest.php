73d38a6c53030aa8e0ada18befe25edb2539bd06
Tyson <Tyson@Tyson-PC>
2015-04-28 14:57:47 -0700

    Fixed risky test for TaxPrice

--- tests/TaxPriceTest.php
+++ tests/TaxPriceTest.php
@@ -4,6 +4,7 @@ class TaxPriceTest extends PHPUnit_Framework_TestCase
 {
     /**
      * @covers TaxPrice::__construct
+     * @uses AbstractPriceModifier
      */
     public function testConstruct()
     {
